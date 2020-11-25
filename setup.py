ca594ffa9e3c94a4d1266d5673ab2bbf81d885d6
Rob Emanuele <rdemanuele@gmail.com>
2020-11-24 19:19:37 -0500

    Update versions for cogeo-mosaic, titiler and rio-cogeo

--- setup.py
+++ setup.py
@@ -18,9 +18,9 @@ install_requires = [
     "sqlakeyset",
     "stac-pydantic>=1.3.5",
     "pydantic[dotenv]",
-    "cogeo-mosaic==3.0a10",
-    "titiler==0.1a2",
-    "rio-cogeo==2.0a5",
+    "cogeo-mosaic==3.0a17",
+    "titiler==0.1a12",
+    "rio-cogeo==2.0.1",
 ]
 
 extra_reqs = {
